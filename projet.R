c34c6507cbf32cfea5210af3e210cd2ea86da908
nananass-ensiie <hanna.benjedidia@gmail.com>
2019-01-07 00:36:14 +0100

    peut etre un début?

--- projet.R
+++ projet.R
@@ -1,17 +1,25 @@
-X = data.frame(read.table("/home/hanna_benj/Documents/semestre5/MAL/caesarian.csv", sep = ",", header = TRUE))
-library(tree)
+rm(list=ls())
+X = data.frame(read.table("/home/hanna_benj/Documents/semestre5/MAL/MAL_projet/caesarian.csv", sep = ",", header = TRUE))
+X$Caesarian <- as.character(X$Caesarian)
+X$Caesarian <- as.factor(X$Caesarian)
+library(naivebayes)
 library(MASS)
+library(randomForest)
+library(ipred)
+library(tree)
+library(ROCR)
 library(DAAG)
+plot(X)
+boxplot(X)
 #tree
 
-Z$Y=as.factor(Z$Y)
 modtree=tree(Caesarian~.,data=X)
 plot(modtree)
 text(modtree)
 
 #faire attention aux codage des variables, coder le 1,2,3,4 des enfants en faisant une colone par enfants au lieu de une colone avec 1234. il y a des package qui le fond. fonction demi coding library nnet
 #class.ind(X$Delivery.number)
-k=10
+k=4
 n = dim(X)[1]
 pas = floor(n/k)
 er1 =c(1:k)
@@ -22,11 +30,25 @@ for (i in 1:k){
   resSim = tree(Caesarian~.,data = tabSim)
   pred = predict(resSim, newdata = tabTest)
   difference= table(pred>0.5,tabTest$Caesarian)
-  er1[i] = (difference[2] + difference[3]) / length(tabTest[,1]) #Erreur totale
-  confusion(tabTest$Caesarian,pred)
+  er1[i] = (difference[2] + difference[3]) / length(tabTest[,1]) #Erreur totaletabSim
 }
 m = mean(er1)
 boxplot(er1)
 p = prediction(pred,tabTest$Caesarian)
 perf = performance(p,"tpr","fpr")
 plot(perf)
+
+# Random Forest
+modFor2 = randomForest(Caesarian~.,data = tabSim)
+pred4 = predict(modFor2,newdata = tabTest, type ="prob")
+p3 = prediction(pred4[,2],spam)
+perf3 = performance(p3,"tpr","fpr")
+plot(perf3, col = "red")
+
+#Bagging
+modbag = bagging(spam~.,data=tabSim,coob=TRUE)
+pred3 = predict(modbag,newdata = tabTest, type ="prob")
+p2 = prediction(pred3[,2],spam)
+perf2 = performance(p2,"tpr","fpr")
+plot(perf2, col = "blue")
+
